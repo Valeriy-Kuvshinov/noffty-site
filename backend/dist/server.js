b849f9b8c2741997d9b5cb06be7adedd758cffdc
Valeriy Kuvshinov <valerykuvshinuv@gmail.com>
2024-07-08 18:21:41 +0300

    getting even closer....

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,5 +1,4 @@
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -12,16 +11,12 @@ import { userRoutes } from './api/user/user.controller.js';
 import { authRoutes } from './api/auth/auth.controller.js';
 import { loggerService } from './services/logger.service.js';
 import { automationService } from './services/automation.service.js';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(cookieParser()); // for res.cookies
 app.use(express.json()); // for req.body
 const rootDir = process.cwd();
 if (process.env.NODE_ENV === 'production') {
-    // Express serve static files on production environment
-    // app.use(express.static(path.resolve(__dirname, '..', 'standalone')))
     // Serve Next.js static files
     app.use('/_next', express.static(path.join(rootDir, 'standalone', '.next')));
 }
@@ -36,26 +31,29 @@ else {
             'http://localhost:3030'
         ],
         credentials: true,
-    }; // Configuring CORS
+    };
     app.use(cors(corsOptions));
 }
+// API routes
 app.use('/api/game', gameRoutes);
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/mail', mailRoutes);
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '..', 'standalone', 'index.html'))
-// })
 // Serve Next.js pages
 app.get('*', (req, res) => {
     const pagePath = path.join(rootDir, 'standalone', '.next', 'server', 'app', req.path, 'index.html');
     console.log(`Attempting to serve: ${pagePath}`);
-    if (fs.existsSync(pagePath))
+    if (fs.existsSync(pagePath)) {
         res.sendFile(pagePath);
+    }
     else {
         console.log(`Falling back to: ${path.join(rootDir, 'standalone', '.next', 'server', 'app', 'index.html')}`);
         res.sendFile(path.join(rootDir, 'standalone', '.next', 'server', 'app', 'index.html'));
     }
+});
+// Handle 404 pages
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(rootDir, 'standalone', '.next', 'server', 'app', '_not-found', 'index.html'));
 });
 const port = process.env.PORT || 3030;
 const server = http.createServer(app);
--- backend/server.ts
+++ backend/server.ts
@@ -15,9 +15,6 @@ import { authRoutes } from './api/auth/auth.controller.js'
 import { loggerService } from './services/logger.service.js'
 import { automationService } from './services/automation.service.js'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
 dotenv.config()
 const app = express()
 
@@ -27,8 +24,6 @@ app.use(express.json()) // for req.body
 const rootDir = process.cwd()
 
 if (process.env.NODE_ENV === 'production') {
-  // Express serve static files on production environment
-  // app.use(express.static(path.resolve(__dirname, '..', 'standalone')))
   // Serve Next.js static files
   app.use('/_next', express.static(path.join(rootDir, 'standalone', '.next')))
 } else {
@@ -42,29 +37,32 @@ if (process.env.NODE_ENV === 'production') {
       'http://localhost:3030'
     ],
     credentials: true,
-  } // Configuring CORS
+  }
   app.use(cors(corsOptions))
 }
 
+// API routes
 app.use('/api/game', gameRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/mail', mailRoutes)
-
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '..', 'standalone', 'index.html'))
-// })
 
 // Serve Next.js pages
 app.get('*', (req, res) => {
   const pagePath = path.join(rootDir, 'standalone', '.next', 'server', 'app', req.path, 'index.html')
   console.log(`Attempting to serve: ${pagePath}`)
 
-  if (fs.existsSync(pagePath)) res.sendFile(pagePath)
-  else {
+  if (fs.existsSync(pagePath)) {
+    res.sendFile(pagePath)
+  } else {
     console.log(`Falling back to: ${path.join(rootDir, 'standalone', '.next', 'server', 'app', 'index.html')}`)
     res.sendFile(path.join(rootDir, 'standalone', '.next', 'server', 'app', 'index.html'))
   }
+})
+
+// Handle 404 pages
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(rootDir, 'standalone', '.next', 'server', 'app', '_not-found', 'index.html'))
 })
 
 const port = process.env.PORT || 3030
