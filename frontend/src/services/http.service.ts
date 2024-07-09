bd69a78f1f1e753a10b6c78baa42340b0653209c
Valeriy Kuvshinov <valerykuvshinuv@gmail.com>
2024-07-09 23:35:55 +0300

    attempt at serving the production at our new VPS, with a different domain

--- frontend/src/services/http.service.ts
+++ frontend/src/services/http.service.ts
@@ -1,6 +1,6 @@
 const baseUrl = process.env.NODE_ENV !== 'production'
     ? 'http://localhost:3030/api/'
-    : 'https://noffty.onrender.com/api/'
+    : 'https://noffty.com/api/'
 
 export const HttpService = {
     request,
